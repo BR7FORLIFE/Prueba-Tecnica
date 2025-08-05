b68f064137c4b458d7389ad274b4e3d9a930b04b
br7forlife <diazpachecobrayan@gmail.com>
2025-08-05 13:11:43 -0500

    finished SecurityConfig configuration

--- 01-to-do-app/ToDo/src/main/java/com/todo/ToDo/security/config/SecurityConfig.java
+++ 01-to-do-app/ToDo/src/main/java/com/todo/ToDo/security/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.todo.ToDo.security.middlewares.JwtMiddleware;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -21,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final JwtMiddleware jwtMiddleware;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -29,7 +32,7 @@ public class SecurityConfig {
                     .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/register", "/api/auth/login").permitAll().anyRequest().authenticated())
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authenticationProvider(authenticationProvider())
-                    .addFilterBefore(null, UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(jwtMiddleware, UsernamePasswordAuthenticationFilter.class)
                     .build();
     }
 
