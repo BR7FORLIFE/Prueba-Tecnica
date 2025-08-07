1ca256c388614fd8e495d116cb7a7fe6279b0088
br7forlife <diazpachecobrayan@gmail.com>
2025-08-07 09:57:17 -0500

    refactor user interface

--- 01-to-do-app/ToDo/src/main/java/com/todo/ToDo/interfaces/UserInterface.java
+++ 01-to-do-app/ToDo/src/main/java/com/todo/ToDo/interfaces/UserInterface.java
@@ -1,9 +1,12 @@
 package com.todo.ToDo.interfaces;
 
+import java.util.List;
+
 import com.todo.ToDo.models.UserModel;
 
 public interface UserInterface {
     void saveUser(UserModel userModel);
     void deleteUser(Long id);
     UserModel getUserById(Long id);
+    List<UserModel> listAllUSers();
 }
