7da650711fbccf5ec86e12ed2e78451e8e260760
br7forlife <diazpachecobrayan@gmail.com>
2025-08-09 08:23:38 -0500

    add some test for jdbc template

--- 01-to-do-app/ToDo/src/test/java/com/todo/ToDo/ToDoApplicationTests.java
+++ 01-to-do-app/ToDo/src/test/java/com/todo/ToDo/ToDoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.todo.ToDo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class ToDoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired(required = false)
+	private JdbcTemplate jdbcTemplate;
 
+	@Test
+	public void jdbcTemplateProve() {
+		assertThat(jdbcTemplate)
+				.as("JdbcTemplate debe estar disponible en el contexto de Spring")
+				.isNotNull();
+	}
 }
