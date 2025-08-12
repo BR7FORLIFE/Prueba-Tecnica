96eae15ae301d6072ba370b30056eb9b60c49b76
br7forlife <diazpachecobrayan@gmail.com>
2025-08-12 11:04:10 -0500

    create reservation model for DB

--- 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/Reservation.java
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/Reservation.java
@@ -1,11 +1,39 @@
 package com.files.__airplane_agendant.models;
 
+import java.util.Date;
+
+import com.files.__airplane_agendant.enums.ReservationEnum;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class Reservation {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(nullable = false)
+    private String originCountry;
+
+    @Column(nullable = false)
+    private String destinyOrigin;
+
+    @Column(nullable = false)
+    private Date travelDate;
+
+    @Column(nullable = false)
+    private ReservationEnum travelPreferencies;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
