f151be5f4a2d3ae1117dc3928ab70b911ea81153
br7forlife <diazpachecobrayan@gmail.com>
2025-08-14 10:13:09 -0500

    convert optional and create a new query for the user

--- 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/repository/UserRepository.java
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/repository/UserRepository.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Repository;
 
 import com.files.__airplane_agendant.models.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    
+    Optional<User> findByUsername(String username);
 }
