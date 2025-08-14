23576611e7af1fb993ce8bccdc5da0c4ba77dfd9
br7forlife <diazpachecobrayan@gmail.com>
2025-08-14 10:12:20 -0500

    enum roles and password atributies

--- 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/User.java
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/User.java
@@ -1,22 +1,32 @@
 package com.files.__airplane_agendant.models;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.files.__airplane_agendant.enums.UserRoles;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -31,6 +41,22 @@ public class User {
     @Column(nullable = false)
     private String passport;
 
+    @Column(nullable = false)   
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRoles roles;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Reservation> reservations;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_" + roles.name());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 }
