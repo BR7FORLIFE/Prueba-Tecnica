c6ff021f343cd61900c2051473626fe61a42eb91
br7forlife <diazpachecobrayan@gmail.com>
2025-08-21 14:18:16 -0500

    create some services

---
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/services/UserServices.java
@@ -0,0 +1,42 @@
+package com.files.__airplane_agendant.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.files.__airplane_agendant.Dtos.users.AuthenticationRequest;
+import com.files.__airplane_agendant.models.User;
+import com.files.__airplane_agendant.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServices {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void saveUser(AuthenticationRequest userRequest){
+        User user = User.builder()
+                    .id(userRequest.getId())
+                    .identification(userRequest.getIdentification())
+                    .passport(userRequest.getPassport())
+                    .password(passwordEncoder.encode(userRequest.getPassword()))
+                    .reservations(null)
+                    .roles(null)
+                    .build();
+
+        userRepository.save(user);
+    }
+
+    public void deleteUserById(UUID id){
+        userRepository.deleteById(id);
+    }
+}
