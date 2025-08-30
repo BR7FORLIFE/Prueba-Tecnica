1224f1195b0b186ba12e5318713567f9b0c4b7ee
br7forlife <diazpachecobrayan@gmail.com>
2025-08-30 09:30:31 -0500

    agg name for tables in postgresql

--- 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/Reservation.java
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/models/Reservation.java
@@ -11,10 +11,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "reservations")
 public class Reservation {
     
     @Id
@@ -36,4 +39,7 @@ public class Reservation {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Version
+    private Long version;
 }
