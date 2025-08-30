a97bf965895487aa76ffc08ab4b8621e528f177f
br7forlife <diazpachecobrayan@gmail.com>
2025-08-30 09:29:01 -0500

    fix identification campus for problems with size decorator

--- 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/Dtos/flights/FligthValidation.java
+++ 02-airplane-agendant/src/main/java/com/files/__airplane_agendant/Dtos/flights/FligthValidation.java
@@ -8,13 +8,13 @@ import lombok.Data;
 
 @Data
 public class FligthValidation {
-    
+
     @NotNull(message = "the passport can't null!")
     private String passport;
 
     @NotNull(message = "the identifacion cant null!")
-    @Size(min = 10)
-    private Integer identification;
+    @Size(min = 10, max = 10, message = "the identificacion not have ten digits")
+    private String identification;
 
     @NotNull(message = "the type of flight not null!")
     private FligthEnum fligthEnum;
