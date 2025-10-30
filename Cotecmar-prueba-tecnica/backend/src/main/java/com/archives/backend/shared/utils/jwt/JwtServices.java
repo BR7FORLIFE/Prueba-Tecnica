276e561a30498d956d611bbfedc4ddba7951345f
br7forlife <diazpachecobrayan@gmail.com>
2025-10-30 16:51:32 -0500

    create jwt service util

---
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/shared/utils/jwt/JwtServices.java
@@ -0,0 +1,78 @@
+package com.archives.backend.shared.utils.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class JwtServices {
+
+    @Value("${jwt.secret}")
+    private String secret_firm;
+
+    private SecretKey secret_key;
+
+    @PostConstruct
+    public void init() {
+        this.secret_key = Keys.hmacShaKeyFor(secret_firm.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateJwt() {
+        Instant timestampIssueAt = Instant.now();
+        Instant timestampExpiration = timestampIssueAt.plusSeconds(3600);
+
+        Date issueAt = Date.from(timestampIssueAt);
+        Date expiration = Date.from(timestampExpiration);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", "");
+        claims.put("rols", List.of());
+
+        return Jwts
+                .builder()
+                .subject(null)
+                .claims(claims)
+                .signWith(this.secret_key)
+                .issuedAt(issueAt)
+                .expiration(expiration)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        try {
+            JwtParser parser = Jwts.parser().verifyWith(this.secret_key).build();
+            return parser.parseSignedClaims(token).getPayload();
+
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new JwtException("Error to extract claims in jwt!");
+        }
+    }
+
+    public String extractUsername(String token) throws Exception {
+        try {
+            String username = (String) extractAllClaims(token).get("username");
+            return username;
+        } catch (Exception e) {
+            throw new Exception("Error to get username of jwt!");
+        }
+    }
+
+    public boolean isExpiredjwt(String token) {
+        return extractAllClaims(token).getExpiration().after(new Date());
+    }
+}
