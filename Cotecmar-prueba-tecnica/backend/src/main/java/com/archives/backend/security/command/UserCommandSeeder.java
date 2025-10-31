848faf1efe9c9c93ce699facc0c01e8963fff61f
br7forlife <diazpachecobrayan@gmail.com>
2025-10-31 11:20:29 -0500

    definied command initializer

---
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/command/UserCommandSeeder.java
@@ -0,0 +1,10 @@
+package com.archives.backend.security.command;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class UserCommandSeeder implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
