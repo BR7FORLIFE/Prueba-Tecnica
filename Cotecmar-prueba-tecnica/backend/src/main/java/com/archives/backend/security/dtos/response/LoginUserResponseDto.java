0c3bb770dd8b341d61f7eb84c159c4357f861ca3
br7forlife <diazpachecobrayan@gmail.com>
2025-11-01 10:21:40 -0500

    change dto

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/dtos/response/LoginUserResponseDto.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/dtos/response/LoginUserResponseDto.java
@@ -1,10 +1,5 @@
 package com.archives.backend.security.dtos.response;
 
-import java.sql.Date;
-import java.util.Set;
-
-import com.archives.backend.security.enums.RolUser;
-
-public record LoginUserResponseDto(String username, Set<RolUser> rols, Date create_at) {
+public record LoginUserResponseDto(String token, String message) {
 
 }
