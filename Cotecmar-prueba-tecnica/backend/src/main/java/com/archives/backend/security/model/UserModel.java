ea6de5fb0d2cf07042eb74617b383f173466320a
br7forlife <diazpachecobrayan@gmail.com>
2025-11-04 12:20:14 -0500

    agg Enumerated

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/model/UserModel.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/model/UserModel.java
@@ -7,6 +7,8 @@ import com.archives.backend.security.enums.RolUser;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,5 +32,6 @@ public class UserModel {
     private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
     private Set<RolUser> rols;
 }
