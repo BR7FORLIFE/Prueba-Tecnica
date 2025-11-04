e5b9c0cc6875377c10adef23ec7d1f44721f7cce
br7forlife <diazpachecobrayan@gmail.com>
2025-11-04 12:20:47 -0500

    remove warning casting

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/shared/utils/helper/ResponseEntityHelper.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/shared/utils/helper/ResponseEntityHelper.java
@@ -1,14 +1,21 @@
 package com.archives.backend.shared.utils.helper;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 
 import com.archives.backend.shared.utils.result.Result;
 
 public class ResponseEntityHelper {
-    static public <T> ResponseEntity<T> toResponseEntity(Result<T, Exception> result) {
-        if (result == null || !result.isOk()) {
-            return (ResponseEntity<T>) ResponseEntity.status(400).body(result != null ? result.getError() : null);
+    static public <T> ResponseEntity<?> toResponseEntity(Result<T, Exception> result) {
+        if (result == null) {
+            return ResponseEntity.internalServerError().body("Internal error: result is null");
         }
+
+        if (!result.isOk()) {
+            return ResponseEntity.badRequest().body(Map.of("message", result.getError().getMessage()));
+        }
+
         return ResponseEntity.ok(result.getValue());
     }
 }
