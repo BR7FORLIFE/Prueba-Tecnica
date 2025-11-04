1b31ec34ef0f58b9bfbc819c66e73b3220af9056
br7forlife <diazpachecobrayan@gmail.com>
2025-11-04 12:18:18 -0500

    refactoring

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/controller/UserController.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/controller/UserController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.archives.backend.security.dtos.request.LoginUserRequestDto;
 import com.archives.backend.security.dtos.request.RegisterUserRequestDto;
-import com.archives.backend.security.dtos.response.LoginUserResponseDto;
-import com.archives.backend.security.dtos.response.RegisterUserResponseDto;
 import com.archives.backend.security.services.UserService;
 import com.archives.backend.shared.utils.helper.ResponseEntityHelper;
-import com.archives.backend.shared.utils.result.Result;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
@@ -26,15 +25,15 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterUserResponseDto> register(@RequestBody @Valid RegisterUserRequestDto dtoUser) {
-        Result<RegisterUserResponseDto, Exception> result = userService.register(dtoUser);
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterUserRequestDto dtoUser) {
+        var result = userService.register(dtoUser);
         return ResponseEntityHelper.toResponseEntity(result);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginUserResponseDto> login(@RequestBody @Valid LoginUserRequestDto dtoUser,
+    public ResponseEntity<?> login(@RequestBody @Valid LoginUserRequestDto dtoUser,
             HttpServletResponse response) {
-        Result<LoginUserResponseDto, Exception> result = userService.login(dtoUser, response);
+        var result = userService.login(dtoUser, response);
         return ResponseEntityHelper.toResponseEntity(result);
     }
 }
