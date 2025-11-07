9e50bcd29f8cf436b147a860638df69b2addc14a
br7forlife <diazpachecobrayan@gmail.com>
2025-11-07 14:51:27 -0500

    add @EnableWebSecurity and @EnableMethodsSecurity

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/config/SecurityConfig.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/config/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +21,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.archives.backend.security.middlewares.JwtFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Autowired
