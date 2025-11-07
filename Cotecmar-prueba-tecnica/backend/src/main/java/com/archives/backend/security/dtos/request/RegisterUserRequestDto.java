22d1c39da6bc3c57db22d34430c2788586c098f9
br7forlife <diazpachecobrayan@gmail.com>
2025-11-07 14:54:06 -0500

    some changes

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/dtos/request/RegisterUserRequestDto.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/security/dtos/request/RegisterUserRequestDto.java
@@ -1,13 +1,8 @@
 package com.archives.backend.security.dtos.request;
 
-import java.util.Set;
-
-import com.archives.backend.security.enums.RolUser;
-
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterUserRequestDto(@NotNull(message = "El username no puede ser nulo!") String username,
-        @NotNull(message = "El password no puede ser nulo!") String password,
-        Set<RolUser> rols) {
+        @NotNull(message = "El password no puede ser nulo!") String password) {
 
 }
