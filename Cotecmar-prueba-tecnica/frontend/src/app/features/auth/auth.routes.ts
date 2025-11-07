a616155dedcb4745c759680f2584b86b36cddaf2
br7forlife <diazpachecobrayan@gmail.com>
2025-11-07 14:52:13 -0500

    add register route

--- Cotecmar-prueba-tecnica/frontend/src/app/features/auth/auth.routes.ts
+++ Cotecmar-prueba-tecnica/frontend/src/app/features/auth/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Login } from '@features/auth/page/login/login';
+import { Register } from '@features/auth/page/register/register';
 
 export const AUTH_ROUTES: Routes = [
   {
@@ -10,5 +11,9 @@ export const AUTH_ROUTES: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
   },
 ];
