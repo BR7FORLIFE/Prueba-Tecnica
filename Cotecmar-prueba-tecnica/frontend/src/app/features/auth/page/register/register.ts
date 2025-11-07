693c3749791f0009dca75c482fc5c405013b7035
br7forlife <diazpachecobrayan@gmail.com>
2025-11-07 14:53:35 -0500

    create http.post to backend

--- Cotecmar-prueba-tecnica/frontend/src/app/features/auth/page/register/register.ts
+++ Cotecmar-prueba-tecnica/frontend/src/app/features/auth/page/register/register.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  NonNullableFormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { Auth } from '@features/auth/service/auth';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
 })
 export class Register {
+  message = '';
+  form!: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+  }>;
 
+  constructor(private fb: NonNullableFormBuilder, private AuthService: Auth) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+      password: this.fb.control('', [Validators.required, Validators.minLength(8)]),
+    });
+  }
+
+  submit(): void {
+    if (this.form.valid) {
+      const { username, password } = this.form.getRawValue();
+      this.AuthService.register(username, password).subscribe();
+    } else {
+      this.form.markAllAsTouched();
+      this.message = 'Por favor corrige los errores';
+    }
+  }
 }
