748aab8d69affd8fbaab83638474668f36a6f2df
br7forlife <diazpachecobrayan@gmail.com>
2025-11-16 09:53:12 -0500

    add new databases methods for get resources

--- Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/features/projects/repository/IProjectRepository.java
+++ Cotecmar-prueba-tecnica/backend/src/main/java/com/archives/backend/features/projects/repository/IProjectRepository.java
@@ -3,8 +3,10 @@ package com.archives.backend.features.projects.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.archives.backend.features.projects.models.ProjectModel;
 
@@ -14,6 +16,9 @@ public interface IProjectRepository extends Repository<ProjectModel, Long> {
     Optional<ProjectModel> findByid(Long id);
 
     Optional<ProjectModel> findByidProject(String idProject);
+
+    @Query("SELECT p FROM ProjectModel p LEFT JOIN FETCH p.blocksModels WHERE p.idProject = :id")
+    Optional<ProjectModel> findProjectWithBlocks(@Param("id") String idProject);
 
     List<ProjectModel> findAll();
 
