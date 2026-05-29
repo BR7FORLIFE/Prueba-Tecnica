fb0409961bdc75b091c662af004de81110e39dc9
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-29 14:37:48 -0500

    feat: add initial Docker setup, Prisma configuration, and application bootstrap

---
+++ constant.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+
+function obtainKeys(path: string) {
+  const key = fs.readFileSync(path, 'utf-8');
+
+  const parserKey = key
+    .replace('-----BEGIN PRIVATE KEY-----', '')
+    .replace('-----END PRIVATE KEY-----', '')
+    .replaceAll('\\s', '');
+
+  return parserKey;
+}
+
+export const PRIVATE_KEY = obtainKeys('./src/config/keys/private.pem');
